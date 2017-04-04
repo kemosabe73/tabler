6a40a426020d598b5206b637a919f957fc33d9d9
kemosabe73 <rajivonarx@gmail.com>
2017-04-04 20:02:20 +0800

    Changed tabler.js

--- tabler.js
+++ tabler.js
@@ -1,3 +1,15 @@
 (function(){
-    //test running
+    $(document).on("click", '.tabler-button', function(){
+        var ctr = 1;
+        var tablerTbl = $(".tabler-table");
+        var tablerTblFr = $(".tabler-table tr:first");
+        tablerTblFr.clone().find("input").each(function(){
+            $(this).attr({
+                'id' : function(_, id) { return id + i },
+                'name' : function(_, name) { return name + i},
+                'value' : null
+            });
+        }).end().appendTo(tablerTbl);
+        ctr++;
+    });
 })();
